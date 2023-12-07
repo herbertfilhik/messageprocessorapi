2da1d5b1fc73ade70eae20e62506e79b943add64
herbertfilhik <herbertfilhik@gmail.com>
2023-12-07 14:45:47 -0300

    initial structure project

---
+++ src/main/java/com/messageprocessorapi/KafkaMessageProcessor/KafkaMessageProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.messageprocessorapi.KafkaMessageProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessageProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messageprocessorapi/KafkaMessageProcessor/KafkaMessageProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messageprocessorapi.KafkaMessageProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessageProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
