5a81247eeb4605282c1a0569b289c2b4cd2ceec6
herbertfilhik <herbertfilhik@gmail.com>
2023-12-07 20:16:00 -0300

    update conf notation to listener kafka

--- src/main/java/com/messageprocessorapi/KafkaMessageProcessor/KafkaMessageProcessorApplication.java
+++ src/main/java/com/messageprocessorapi/KafkaMessageProcessor/KafkaMessageProcessorApplication.java
@@ -2,8 +2,10 @@ package com.messageprocessorapi.KafkaMessageProcessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.messageprocessorapi"})
 public class KafkaMessageProcessorApplication {
 
 	public static void main(String[] args) {
