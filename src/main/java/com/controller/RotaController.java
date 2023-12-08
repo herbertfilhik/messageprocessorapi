fad9ab4178d9384d60f5a86f584a9220a884fd65
herbertfilhik <herbertfilhik@gmail.com>
2023-12-07 23:47:01 -0300

    Applying the API-first approach

--- src/main/java/com/controller/RotaController.java
+++ src/main/java/com/controller/RotaController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.modelo.Rota;
 import com.service.RotaKafkaConsumer;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/rotas")
 public class RotaController {
@@ -19,6 +21,7 @@ public class RotaController {
    private RotaKafkaConsumer rotaKafkaConsumer;
 
    @GetMapping("/concluidas")
+   @Operation(summary = "Obter rotas concluídas", description = "Retorna uma lista de rotas concluídas.")
    public ResponseEntity<List<Rota>> obterRotasCompletas() {
        List<Rota> rotasCompletas = rotaKafkaConsumer.obterRotasCompletas();
        return ResponseEntity.ok(rotasCompletas);
---
+++ src/main/java/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springdoc.core.SwaggerUiConfigProperties;
+
+@Configuration
+@EnableWebMvc
+public class OpenApiConfig implements WebMvcConfigurer {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+            .group("api")
+            .pathsToMatch("/rotas/**") // Especifique os caminhos da API que deseja documentar
+            .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
+            .resourceChain(false);
+
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public SwaggerUiConfigParameters swaggerUiConfigParameters(SwaggerUiConfigProperties swaggerUiConfigProperties) {
+        return new SwaggerUiConfigParameters(swaggerUiConfigProperties);
+    }
+}
