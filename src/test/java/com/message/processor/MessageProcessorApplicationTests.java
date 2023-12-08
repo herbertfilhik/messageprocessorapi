c6f5ed19056d4c3b14c15a607a826a0aed196110
herbertfilhik <herbertfilhik@gmail.com>
2023-12-08 02:16:30 -0300

    test class to use with junit5

--- src/test/java/com/message/processor/MessageProcessorApplicationTests.java
+++ src/test/java/com/message/processor/MessageProcessorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.messageprocessorapi.KafkaMessageProcessor;
+package com.message.processor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaMessageProcessorApplicationTests {
+class MessageProcessorApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/com/message/processor/MyUnitTest.java
@@ -0,0 +1,12 @@
+package com.message.processor;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyUnitTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }
+}
